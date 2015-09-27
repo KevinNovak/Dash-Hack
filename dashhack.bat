e8387e0bf4890d6f252b28b30acecd9d5cbf6beb
Kevin Novak <kevinmnovak@yahoo.com>
2015-09-27 16:29:30 -0400

    Fixed some delay problems

--- dashhack.bat
+++ dashhack.bat
@@ -12,26 +12,25 @@ for /f %%A in ('"prompt $H &echo on &for %%B in (1) do rem"') do set BS=%%A
 
 cd C:\Users\kevin\Desktop\wireshark\App\Wireshark
 
-start tshark -i 4 -p -w %TEMP%\dashlisten.txt host 255.255.255.255
+start tshark -i 4 -p -w %TEMP%\dashlisten.txt host 255.255.255.255 and 192.168.0.1
 
 ping 1.1.1.1 -n 1 -w 3000 > nul
 
 setlocal
-set file="C:\Users\kevin\Desktop\hello.txt"
-set oldSize=276
+set oldSize=500
 :_loop
-ping 1.1.1.1 -n 1 -w 1500 > nul
+ping 1.1.1.1 -n 1 -w 2000 > nul
 FOR /F "usebackq" %%A IN ('%TEMP%\dashlisten.txt') DO set size=%%~zA
 
 if %size% GTR %oldsize% (
     echo Button Pushed.
-    set oldSize=%size%
     goto _playsound
 )
 goto _loop
 
 :_playsound
 C:\Users\Kevin\Documents\GitHub\Dash-Hack\resources\sWavPlayer.exe C:\Users\Kevin\Documents\GitHub\Dash-Hack\resources\rickroll.wav
+set oldSize=%size%
 goto _loop
 
 pause
