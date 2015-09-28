6c2862aed487823abf06b62b6e55e5471d587c3e
Kevin Novak <kevinmnovak@yahoo.com>
2015-09-28 11:26:44 -0400

    Starts and Kill Capture Process

--- dashhack.bat
+++ dashhack.bat
@@ -7,28 +7,22 @@
 
 title Dash Hack
 
-:: define a variable containing a single backspace character
-for /f %%A in ('"prompt $H &echo on &for %%B in (1) do rem"') do set BS=%%A
-
 start tshark -i 4 -p -w %TEMP%\dashlisten.txt host 255.255.255.255 and 192.168.0.1
-
+echo Please ^Wait
 ping 1.1.1.1 -n 1 -w 3000 > nul
+echo Begin
+FOR /F "usebackq" %%A IN ('%TEMP%\dashlisten.txt') DO set defaultsize=%%~zA
 
-setlocal
-set oldSize=500
 :_loop
-ping 1.1.1.1 -n 1 -w 4000 > nul
 FOR /F "usebackq" %%A IN ('%TEMP%\dashlisten.txt') DO set size=%%~zA
-
-if %size% GTR %oldsize% (
-    echo Button Pushed.
-    goto _playsound
+if %size% GTR %defaultsize% (
+    start sWavPlayer.exe rickroll.wav
+    taskkill /f /im tshark.exe
+    start tshark -i 4 -p -w %TEMP%\dashlisten.txt host 255.255.255.255 and 192.168.0.1
+    ping 1.1.1.1 -n 1 -w 18000 > nul
+    goto _loop
 )
-goto _loop
-
-:_playsound
-sWavPlayer.exe rickroll.wav
-set oldSize=%size%
+ping 1.1.1.1 -n 1 -w 600 > nul
 goto _loop
 
 pause
